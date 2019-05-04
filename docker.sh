1ecfe994a83fe4ffe2cd72b1697ebf282d9e5ed8
gvlaan <44431040+gvlaan@users.noreply.github.com>
2019-05-04 16:32:15 +0200

    Delete docker.sh

--- docker.sh
+++
@@ -1,6 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce --assume-yes --force-yes
